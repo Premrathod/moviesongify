ecce0feb7447c7365b5110ae3b9d56fb2bcfe3dc
PatkarAditya22 <adityapatkar22@gmail.com>
2021-08-17 19:18:05 +0530

    added TMDB API and 2 routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    console.log("user");
-    res.render("user/home");
+router.get("/",async(req,res)=>{
+    let response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`);
+    response = await response.json();
+    res.render("user/home",{topRated : response.results});
+})
+
+router.get("/movie/:id",async(req,res)=>{
+    let response = await fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=${process.env.TMDB_API_KEY}`);
+    response = await response.json();
+    console.log(response);
+    res.render("user/movie",{movie:response});
 })
 
 module.exports = router;
